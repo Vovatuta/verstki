ea825ea2113c49fcb044b738d4ccdbf79afd1af5
unknown <volodymyrtuta@gmail.com>
2023-05-15 19:53:41 +0300

    pach 1.0

--- my game/main.js
+++ my game/main.js
@@ -2,6 +2,7 @@ import { Player } from './player.js';
 import { InputHandler } from './input.js';
 import { Background } from './background.js';
 import { FlyingEnemy, ClimbingEnemy, GroundEnemy } from './enemies.js';
+import { UI } from './UI.js';
 
 window.addEventListener('load', function(){
 const canvas = document.getElementById('canvas1');
@@ -18,10 +19,18 @@ class Game {
     this.maxSpeed = 3;
     this.background = new Background(this);
     this.player = new Player(this);
-    this.input = new InputHandler();
+    this.input = new InputHandler(this);
+    this.UI = new UI(this);
     this.enemies = [];
+    this.particles = [];
+    this.maxParticles = 200;
     this.enemyTimer = 0;
     this.enemyInterval = 1000;
+    this.debug = true;
+    this.score = 0;
+    this.fontColor = 'black';
+    this.player.currentState = this.player.states[0];
+    this.player.currentState.enter();
     
   }
       update(deltaTime){
@@ -38,14 +47,26 @@ class Game {
           enemy.update(deltaTime);
           if (enemy.markedForDeletion) this.enemies.splice(this.enemies.indexOf(enemy), 1);
         });
-
+          // handle particle
+          this.particles.forEach((particle, index) => {
+           particle.update();
+           if (particle.markedForDeletion) this.particles.splice(index, 1);
+          });
+          if (this.particles.length > this.maxParticles){
+            this.particles = this.particles.slice(0, this.maxParticles);
+          }
+          
         }
       draw(context){
         this.background.draw(context);
         this.player.draw(context);
         this.enemies.forEach(enemy => {
           enemy.draw(context);
-        })
+        });
+        this.particles.forEach(particle => {
+          particle.draw(context);
+        });
+        this.UI.draw(context);
       }
       addEnemy(){
         if (this.speed > 0 && Math.random() < 0.5) this.enemies.push(new GroundEnemy(this));
@@ -56,7 +77,6 @@ class Game {
         }
 }
 const game = new Game(canvas.width, canvas.height);
-console.log(game);
 let lastTime = 0;
 
 function animate(timeStamp){
--- my game/player.js
+++ my game/player.js
@@ -1,4 +1,4 @@
-import { Sitting, Running, Jumping, Falling } from './playerStates.js';
+import { Sitting, Running, Jumping, Falling, Rolling, Diving} from './playerStates.js';
 export class Player {
     constructor(game){
       this.game = game;
@@ -17,11 +17,10 @@ export class Player {
       this.frameTimer = 0;
       this.speed = 0;
       this.maxSpeed = 10;
-      this.states = [new Sitting(this), new Running(this), new Jumping(this), new Falling(this)];
-      this.currentState = this.states[0];
-      this.currentState.enter();
+      this.states = [new Sitting(this.game), new Running(this.game), new Jumping(this.game), new Falling(this.game), new Rolling(this.game), new Diving(this.game)];
     }
     update(input, deltaTime){
+      this.checkCollision();
       this.currentState.handleInput(input);
       // horizontal movement
       this.x += this.speed;
@@ -48,6 +47,7 @@ export class Player {
 
     }
     draw(context){
+      if (this.game.debug) context.strokeRect(this.x, this.y, this.width, this.height);
       context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height );
     }
     onGround(){
@@ -57,5 +57,22 @@ export class Player {
       this.currentState = this.states[state];
       this.game.speed = this.game.maxSpeed * speed;
       this.currentState.enter();
+    }
+    checkCollision(){
+      this.game.enemies.forEach(enemy => {
+        if (
+          enemy.x < this.x + this.width &&
+          enemy.x + enemy.width > this.x &&
+          enemy.y < this.y + this.height &&
+          enemy.y + enemy.height > this.y
+        ){
+          enemy.markedForDeletion = true;
+          this.game.score++;
+        } else{
+          // no colission
+
+        }
+      });
+
     }
 }
